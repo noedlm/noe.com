811e15b66136ba94de86086e9b99e3b38db6feca
Noe De La Mora <noedelamora@gmail.com>
2016-03-25 13:08:07 -0500

    initial commit (again)
    setting up the framework

---
+++ noe_on_the_web/noe_on_the_web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for noe_on_the_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noe_on_the_web.settings")
+
+application = get_wsgi_application()
