f2282d7b9babb932868ca361e1ff167f0a3ba29c
Noe De La Mora <noedelamora@gmail.com>
2016-11-13 23:18:46 -0600

    up to part2 API section.

--- noe_on_the_web/polls/urls.py
+++ noe_on_the_web/polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^test/', views.test, name='test'),
     url(r'^$', views.index, name='index'),
+    url(r'.*', views.not_found, name='404'),
 ]
--- noe_on_the_web/polls/views.py
+++ noe_on_the_web/polls/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpRequest("Hello world! you're at the polls index.")
+    a = "hello world"
+    b = " from the new polls/index!"
+    return HttpResponse(a + b)
+
+def test(request):
+    return HttpResponse("returning the test view from the polls app!")
+
+def not_found(request):
+    return HttpResponse("Page not found!")
