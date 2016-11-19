9c1cd0d7fcc3552ee873bdd74624915c643ff7bb
Noe De La Mora <noedelamora@gmail.com>
2016-11-18 23:33:30 -0600

    rest of tutorial 3
    stopped at https://docs.djangoproject.com/en/1.10/intro/tutorial04/

--- noe_on_the_web/polls/urls.py
+++ noe_on_the_web/polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^test/', views.test, name='test'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
