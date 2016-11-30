5f4e98e6f1af6dc58bd5e64af38f6887da78a0fa
Noe De La Mora <noedelamora@gmail.com>
2016-11-29 21:10:19 -0600

    finished the rest of tutorial part 4, yay!!

--- noe_on_the_web/polls/urls.py
+++ noe_on_the_web/polls/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^test/', views.test, name='test'),
-    url(r'^(?P<question_id>[0-9]+)/*$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/*$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/*$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/*$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/*$', views.vote, name='vote'),
-    url(r'^$', views.index, name='index'),
-    url(r'.*', views.not_found, name='404'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 ]
--- noe_on_the_web/polls/views.py
+++ noe_on_the_web/polls/views.py
@@ -2,27 +2,30 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 
 from .models import Question, Choice
 
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
@@ -39,11 +42,3 @@ def vote(request, question_id):
         selected_choice.save()
 
     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def test(request):
-    return HttpResponse("returning the test view from the polls app!")
-
-
-def not_found(request):
-    return HttpResponse("Page not found!")
