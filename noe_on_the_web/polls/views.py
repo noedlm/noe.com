6e791b97da313facc1a4a3c913ce3a9da84f1fa4
Noe De La Mora <noedelamora@gmail.com>
2016-11-29 20:37:53 -0600

    adding forms and having a beer
    this commit goes up to:
    
    https://docs.djangoproject.com/en/1.10/intro/tutorial04/#use-generic-vie
    ws-less-code-is-better

--- noe_on_the_web/polls/views.py
+++ noe_on_the_web/polls/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F
+from django.urls import reverse
 
-from .models import Question
+
+from .models import Question, Choice
 
 
 # Create your views here.
@@ -18,12 +21,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    context = {'results': "you're looking at the results of question %s" % question_id}
-    return render(request, 'polls/results.html', context)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return render(request, 'polls/vote.html', {'vote': "these are the votes for question %s" % question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 def test(request):
