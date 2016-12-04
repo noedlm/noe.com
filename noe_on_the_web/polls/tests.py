46cf44453ce212afbf4a921aac445050ae431585
Noe De La Mora <noedelamora@gmail.com>
2016-12-03 20:42:08 -0600

    created 2 additional tests
    Stopped at:
    https://docs.djangoproject.com/en/1.10/intro/tutorial05/#the-django-test
    -client

--- noe_on_the_web/polls/tests.py
+++ noe_on_the_web/polls/tests.py
@@ -19,3 +19,23 @@ class QuestionMethodTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions
+        whose pub_date is older than 1 day.
+        :return:
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for questions
+        whose pub_date is less than a day.
+        :return:
+        """
+        time = timezone.now() - datetime.timedelta(hours=3)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
