27644c30500f179a3d461145afd7847802e04110
Noe De La Mora <noedelamora@gmail.com>
2016-12-04 03:26:10 -0600

    finished django writing tests
    Finished part 5. On to part 6 after reading up a bit more python and
    tests

--- noe_on_the_web/polls/views.py
+++ noe_on_the_web/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F
+from django.utils import timezone
 from django.urls import reverse
 from django.views import generic
 
@@ -14,18 +15,38 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        )
+
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        )
 
 
 def vote(request, question_id):
