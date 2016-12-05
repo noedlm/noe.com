7cba7ad76f4067df5511abce4ae8cb78ad097f42
Noe De La Mora <noedelamora@gmail.com>
2016-12-04 23:08:10 -0600

    finishing part 7 customizing the admin site
    stopped here:
    https://docs.djangoproject.com/en/1.10/intro/tutorial07/#customizing-you
    r-project-s-templates

--- noe_on_the_web/polls/admin.py
+++ noe_on_the_web/polls/admin.py
@@ -5,5 +5,20 @@ from .models import Question, Choice
 
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
