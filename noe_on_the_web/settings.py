308e04bf2a5f2b3e4e9545f60b985abc3fe0f586
Noe De La Mora <Noe.DeLaMora@univarusa.com>
2017-03-01 11:44:11 -0600

    more setup. no idea what I'm doing.

--- noe_on_the_web/settings.py
+++ noe_on_the_web/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'collect_static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'polls/static/'),
     os.path.join(BASE_DIR, 'static/'),
