622c2bf7658763f0877795635e8778d7ed67d1d1
Noe De La Mora <Noe.DeLaMora@univarusa.com>
2017-03-24 17:14:55 -0500

    starting to make api calls to blizzard, for science

--- raidCount/views.py
+++ raidCount/views.py
@@ -12,8 +12,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    url = 'http://httpbin.org/get'
-    r = requests.get(url)
-    blizz = os.getenv('BLIZZARD_WEB_API_URL')
-    return HttpResponse(blizz)
+    binUrl = 'http://httpbin.org/get'
+    url = os.getenv('BLIZZARD_WEB_API_URL') + '/wow/guild/Thunderhorn/Fallen Empire'
+    url_params = {
+        'fields': 'members',
+        'locale': 'en_US',
+        'apikey': os.getenv('BLIZZARD_WEB_API_PUBLIC_KEY')
+    }
+    r = requests.get(url, params=url_params)
+    return HttpResponse(r.text)
 
